02bfe5b0b94139d841dcbda84eef0b35b8101481
myj4513 <yejungod@gmail.com>
2022-03-20 21:52:37 +0900

    Update HelloController.java

--- src/main/java/com/study/jenkinspractice/controller/HelloController.java
+++ src/main/java/com/study/jenkinspractice/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+    
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "Hello World2!";
+    }
 }
